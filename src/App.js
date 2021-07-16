6cada9a58d3d5176dec1fe873fca0ca4d81e0646
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-07-16 13:40:00 +0300

    add form for name and number

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import ContactForm from "./componenets/ContactForm/ContactForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+
+  formSubmitHandler = ({ name, number }) => {
+    const contact = {
+      name,
+      number,
+      id: uuidv4(),
+    };
+    this.state.contacts.find((savedContact) => savedContact.name === name)
+      ? alert(`${name} is already in contacts`)
+      : this.setState((prevState) => ({
+          contacts: [...prevState.contacts, contact],
+        }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm handleSubmit={this.formSubmitHandler} />
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
